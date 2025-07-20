53a96c888f926710f7356ce02085e68cfa0e9dfc
Florian Van Belleghem <florian.vanbelleghem@flux.be>
2025-07-20 14:21:14 +0200

    feat: setup blog structure

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
