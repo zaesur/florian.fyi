37f99fea411724f51eacfde563e5f28980deeb2b
Florian Van Belleghem <florian.vanbelleghem@flux.be>
2025-07-20 14:57:16 +0200

    fix: enable output: export

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: "export",
+  output: "export",
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
