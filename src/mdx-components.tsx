c0382da5189a7094bac4b6b8816a966a0f68cac1
Florian Van Belleghem <florian.vanbelleghem@flux.be>
2025-07-21 18:44:19 +0200

    feat: add noise, font

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -3,11 +3,14 @@ import Link from "next/link";
 
 const components: MDXComponents = {
   // Custom components
-  h1: ({ children }) => (
-    <h1 className="text-2xl font-bold text-red-500">{children}</h1>
-  ),
+  h1: ({ children }) => <h1 className="text-2xl font-bold">{children}</h1>,
+  h2: ({ children }) => <h2 className="text-xl font-bold">{children}</h2>,
+  h3: ({ children }) => <h3 className="text-lg font-bold">{children}</h3>,
+  h4: ({ children }) => <h4 className="text-base font-bold">{children}</h4>,
+  h5: ({ children }) => <h5 className="text-sm font-bold">{children}</h5>,
+  h6: ({ children }) => <h6 className="text-xs font-bold">{children}</h6>,
   a: ({ children, href }) => {
-    const className = "text-blue-500";
+    const className = "text-blue-500 hover:text-blue-600 hover:italic";
 
     // If the link is a relative link, use the Link component
     if (href?.startsWith("/")) {
