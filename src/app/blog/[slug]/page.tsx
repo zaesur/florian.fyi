477c146a96b625507f9d8c63ace093d75ba1fdc6
Florian Van Belleghem <florian.vanbelleghem@flux.be>
2025-07-21 14:37:27 +0200

    feat: inject components

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,21 +1,23 @@
+import * as components from "@/components";
 import { getArticleContent, getArticleMetadata, getArticleSlugs } from "@/lib/articles";
 import { MDXRemote } from "next-mdx-remote-client/rsc";
 
 type Params = { slug: string };
+type PageProps = { params: Promise<Params> };
 
-export async function generateStaticParams(): Promise<Params[]> {
+export async function generateStaticParams() {
   const slugs = getArticleSlugs();
   return slugs.map((slug) => ({ slug }));
 }
 
-export async function generateMetadata({ params }: { params: Promise<Params> }) {
+export async function generateMetadata({ params }: PageProps) {
   const { slug } = await params;
-  const metadata = getArticleMetadata(slug);
+  const metadata = getArticleMetadata<{ title: string }>(slug);
   return { title: metadata.title };
 }
 
-export default async function Page({ params }: { params: Promise<Params> }) {
+export default async function Page({ params }: PageProps) {
   const { slug } = await params;
   const content = getArticleContent(slug);
-  return <MDXRemote source={content} />;
+  return <MDXRemote source={content} components={components} />;
 }
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { App as LusionScene } from "./LusionScene";
