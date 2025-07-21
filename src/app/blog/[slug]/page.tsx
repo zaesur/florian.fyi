92f8a0d97d2ab08e91b4312fb47157ca0bb7fb58
Florian Van Belleghem <florian.vanbelleghem@flux.be>
2025-07-21 14:19:43 +0200

    feat: add title from gray matter

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { getArticleContent, getArticleSlugs } from "@/lib/articles";
+import { getArticleContent, getArticleMetadata, getArticleSlugs } from "@/lib/articles";
 import { MDXRemote } from "next-mdx-remote-client/rsc";
 
 type Params = { slug: string };
@@ -6,6 +6,12 @@ type Params = { slug: string };
 export async function generateStaticParams(): Promise<Params[]> {
   const slugs = getArticleSlugs();
   return slugs.map((slug) => ({ slug }));
+}
+
+export async function generateMetadata({ params }: { params: Promise<Params> }) {
+  const { slug } = await params;
+  const metadata = getArticleMetadata(slug);
+  return { title: metadata.title };
 }
 
 export default async function Page({ params }: { params: Promise<Params> }) {
